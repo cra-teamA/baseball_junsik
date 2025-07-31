0a3dea4c7bf11512708d467c799174ce1c4913dc
junsik <junsik@ma.il>
2025-07-31 14:45:57 +0900

    [refactor] p.50 파이썬스럽게,

--- game.py
+++ game.py
@@ -1,16 +1,21 @@
+from gameresult import GameResult
+
+
 class Game:
+
+    def _assert_illegal_value(self, guess_number):
+        if guess_number is None:
+            raise TypeError("입력이 None 이네여?")
+        if len(guess_number) != 3:
+            raise TypeError("입력이 3자리가 아니네여?")
+        if not guess_number.isdigit():
+            raise TypeError("입력이 숫자가 아니에여?")
+        if self._is_duplicate_number(guess_number):
+            raise TypeError("입력이 중복 숫자가 있네여")
+
     def _is_duplicate_number(self, guess_number: str):
         return len(set(guess_number)) != 3
 
-    def guess(self, guessNumber: str):
-        if guessNumber is None:
-            raise TypeError("입력이 None 이네여?")
-        if len(guessNumber) != 3:
-            raise TypeError("입력이 3자리가 아니네여?")
-
-        if not guessNumber.isdigit():
-            raise TypeError("입력이 숫자가 아니에여?")
-
-        if self._is_duplicate_number(guessNumber):
-            raise TypeError("입력이 중복 숫자가 있네여")
-?
+    def guess(self, guess_number: str):
+        self._assert_illegal_value(guess_number)
+        return GameResult(True,3,0)
--- test_game.py
+++ test_game.py
@@ -1,11 +1,11 @@
 import pytest
-
 from game import Game
+from gameresult import GameResult
 
 
 @pytest.fixture
 def game():
-    return Game
+    return Game()
 
 
 def assert_illegal_argument(game, guess_number):
@@ -16,9 +16,16 @@ def assert_illegal_argument(game, guess_number):
         pass
 
 
-def test_exeption_when_invalid_input(game):
-    assert_illegal_argument(game, None)
-    assert_illegal_argument(game, "12")
-    assert_illegal_argument(game, "1234")
-    assert_illegal_argument(game, "12s")
-    assert_illegal_argument(game, "121")
+@pytest.mark.parametrize('invalid_input', [None, '12', '1234', '12s', '121'])
+def test_exeption_when_invalid_input(game, invalid_input):
+    assert_illegal_argument(game, invalid_input)
+
+
+def test_return_solved_result_if_matched_number(game):
+    game.question = '123'
+    result: GameResult = game.guess("123")
+
+    assert result is not None
+    assert result.solved == True
+    assert result.strikes == 3
+    assert result.balls == 0
