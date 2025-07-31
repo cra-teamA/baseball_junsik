c15ddd9824f8371f7b6d27dd1c0274b9a6af719b
junsik <junsik@ma.il>
2025-07-31 14:14:06 +0900

    [feature] p.31 len test 추가

--- test_game.py
+++ test_game.py
@@ -2,6 +2,7 @@ import pytest
 
 from game import Game
 
+
 @pytest.fixture
 def game():
     return Game
@@ -11,3 +12,9 @@ def test_exeption_when_input_is_none(game):
     game = Game()
     with pytest.raises(TypeError):
         game.guess(None)
+
+
+def test_exeption_when_input_len_unmatched(game):
+    game = Game()
+    with pytest.raises(TypeError):
+        game.guess("12")
